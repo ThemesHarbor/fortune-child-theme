e55f9946618e106e4bb0b4a8a24ef9f642e9a3eb
Taras Dashkevych <trdashix@gmail.com>
2018-11-27 16:49:25 -0800

    Remove: enqueue parent style function (now this style is loaded via the parent theme);

--- functions.php
+++ functions.php
@@ -14,17 +14,7 @@
   *
   * @link http://codex.wordpress.org/Child_Themes
   */
- function fortune_child_theme_setup() {
+function fortune_child_theme_setup() {
      load_child_theme_textdomain( 'fortune', get_stylesheet_directory() . '/languages' );
- }
- add_action( 'after_setup_theme', 'fortune_child_theme_setup' );
-
-/**
- * Enqueue parent style.
- *
- * @link http://codex.wordpress.org/Child_Themes
- */
-function fortune_child_enqueue_parent_styles() {
-    wp_enqueue_style( 'fortune-parent-style', get_template_directory_uri() . '/style.css', array( 'fortune-css-framework', 'font-awesome' ) );
 }
-add_action( 'wp_enqueue_scripts', 'fortune_child_enqueue_parent_styles' );
+add_action( 'after_setup_theme', 'fortune_child_theme_setup' );
